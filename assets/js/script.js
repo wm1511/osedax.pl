d47454aa8bb1e3da9ea402b6e2818aba0a831d26
Wiktor Merta <wiktor.merta@op.pl>
2024-11-26 13:31:56 +0100

    Preloader, contact tab

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     $('preloader').fadeOut();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.style.display = "none"; // Hide preloader
+});
