8d95856e9a6574ef57a2d34794f5f346afd4abcc
Wiktor Merta <wiktor.merta@op.pl>
2024-11-26 22:12:45 +0100

    Finished preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-// $(document).ready(function() {
-//     $('preloader').fadeOut();
-// });
-
-document.addEventListener("DOMContentLoaded", function () {
-    const preloader = document.getElementById("preloader");
-    preloader.style.display = "none"; // Hide preloader
+$(window).on('load', function () {
+    $('.preloader').fadeOut(500);
 });
