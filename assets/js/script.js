b1d15135648ae6e3af256ffb8ec1dd5953b99d42
Wiktor Merta <wiktor.merta@op.pl>
2024-12-12 11:02:45 +0100

    Added animations and more content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-// Preloader
-window.addEventListener('DOMContentLoaded', function () {
-    const preloader = document.querySelector('.preloader');
-    if (preloader) {
-        preloader.style.transition = 'opacity 0.2s';
-        preloader.style.opacity = '0';
-
-        setTimeout(() => {
-            preloader.style.display = 'none';
-            preloader.style.visibility = 'hidden';
-        }, 200);
-    }
-});
-
 // Map
 if (document.getElementById('map')) {
     const map = L.map('map').setView([49.9329121, 19.3855164], 15);
@@ -58,5 +44,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Animate on scroll
-AOS.init();
+// sal
+sal();
